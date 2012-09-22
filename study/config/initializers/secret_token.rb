653a936095ec5b61d9713c18634548a86473124d
morishita <tomohiro.m0219@gmail.com>
2012-09-22 22:34:41 +0900

    railsの練習用アプリケーションのインストール

---
+++ study/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Study::Application.config.secret_token = '5f275a9c626dc1d1b4856904cfd7e98001ff28ece811fa47d804f30411a5df6d33b1b2d77b31f172be7f730582003a664f0ab779cd2c314787c9551e6bba0036'
