5f60c54b20b52fb0bbf264101705975591d76e80
morishita <tomohiro.m0219@gmail.com>
2012-09-23 21:53:57 +0900

    リポジトリ直下にrailsを再インストール

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudyRails::Application.config.secret_token = '4f2cc85fb364a9dcf7fe51690e63084641e5cbdc1b83d699dd7c4d6248ba750f06c0dcbcdacd25943529586bfc01e71434c596bc81944e8e79c9858d8a7f4710'
